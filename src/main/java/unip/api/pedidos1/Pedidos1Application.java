0039492e93125f2160c2c4b288e4c494a3da6294
gustavo <gustavogaldinojj@gmail.com>
2026-09-19 22:38:43 -0300

    apiPedidos

---
+++ src/main/java/unip/api/pedidos1/Pedidos1Application.java
@@ -0,0 +1,13 @@
+package unip.api.pedidos1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pedidos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pedidos1Application.class, args);
+	}
+
+}
---
+++ src/test/java/unip/api/pedidos1/Pedidos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package unip.api.pedidos1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pedidos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
